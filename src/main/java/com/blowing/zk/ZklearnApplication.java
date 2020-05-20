66ebbc89ad27166ba5eb1704f2b4cf6c6ae1e5a7
wujie <1072878103@qq.com>
2020-05-20 13:53:37 +0800

    Initial commit

---
+++ src/main/java/com/blowing/zk/ZklearnApplication.java
@@ -0,0 +1,13 @@
+package com.blowing.zk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZklearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZklearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blowing/zk/ZklearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blowing.zk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZklearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
